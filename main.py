e7353fe3d50e76600a1c558587d02b62519aab1e
vltrevenger <slavadan909@gmail.com>
2021-05-04 16:45:50 +0300

    diagElements

--- main.py
+++ main.py
@@ -4,15 +4,53 @@ def neighbourhood(list, x, y):
 
         res = []
 
+        if y-1 >= 0 and x-1 >= 0:
+            res.append(list[y-1][x-1])
+        else:
+            res.append(None)
+
         if y-1 >= 0:
             res.append(list[y-1][x])
-        if y+1 < len(list):
-            res.append(list[y+1][x])
+        else:
+            res.append(None)
+
+
+        if y-1 >= 0 and x+1 < len(list[y-1]):
+            res.append(list[y-1][x+1])
+        else:
+            res.append(None)
 
         if x-1 >= 0:
             res.append(list[y][x-1])
+        else:
+            res.append(None)
+
+
+
+
         if x+1 < len(list[y]):
             res.append(list[y][x+1])
+        else:
+            res.append(None)
+
+
+
+        if y+1 < len(list) and x-1 >= 0:
+            res.append(list[y+1][x-1])
+        else:
+            res.append(None)
+
+        if y+1 < len(list):
+            res.append(list[y+1][x])
+        else:
+            res.append(None)
+
+        if y+1 < len(list) and x+1 < len(list[y+1]):
+            res.append(list[y+1][x+1])
+        else:
+            res.append(None)
+
+
 
         return res
 
@@ -31,7 +69,8 @@ for i in range(len(list)):
     print(list[i])
 
 
-
 chooseX = int(input("Choose X coord: "))
 chooseY = int(input("Choose Y coord: "))
-print(neighbourhood(list, chooseX, chooseY))
+
+res = neighbourhood(list, chooseX, chooseY)
+print(res)
